d7a8dfb6d1d7511e50845754ec10f925e7028376
Marek Lis <maxymilian.black@gmail.com>
2016-05-09 11:47:59 +0200

    switch back to vue

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var elixir = require('laravel-elixir');
-var elixirTypscript = require('elixir-typescript');
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -14,19 +13,5 @@ var elixirTypscript = require('elixir-typescript');
 elixir(function(mix) {
     mix
         .sass('app.scss')
-        .copy('node_modules/@angular', 'public/@angular')
-        .copy('node_modules/rxjs', 'public/rxjs')
-        .copy('node_modules/systemjs', 'public/systemjs')
-        .copy('node_modules/es6-shim', 'public/es6-shim')
-        .copy('node_modules/zone.js/dist', 'public/zone')
-        .typescript('boot.js','public','/**/*.ts',{
-            "target": "ES5",
-            "module": "system",
-            "moduleResolution": "node",
-            "sourceMap": true,
-            "emitDecoratorMetadata": true,
-            "experimentalDecorators": true,
-            "removeComments": false,
-            "noImplicitAny": false
-        });
+        .browserify('unicorn.js')
 });
--- resources/assets/typescript/boot.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../../typings/main.d.ts" />
-
-import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component'
-
-bootstrap(AppComponent);
